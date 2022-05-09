d26384270666800fb1a55d099246b22490a99f55
gavvahar <nihargavva@gmail.com>
2022-05-09 14:05:28 -0400

    testing to see if the buttons work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,36 @@
-// TODO: Declare any global variables we need
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+(function()
+{
+    const querySelectorArr = [
+        document.querySelector('#flip'),
+        document.querySelector('#clear'),
+        document.querySelector('#penny-image'),
+        document.querySelector('#message'),
+        document.querySelector('#heads'),
+        document.querySelector('#heads-percent'),
+        document.querySelector('#tails'),
+        document.querySelector('#tails-percent')
+    ];
+    function setUpEventListeners()
+    {
+        for(let i = 0; i < querySelectorArr.length; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                    querySelectorArr[i].addEventListener('click', function(){
+                        console.log('you clicked flip');
+                    });
+                    break;
+                case 1:
+                    querySelectorArr[i].addEventListener('click', function(){
+                        console.log('you clicked clear');
+                    });
+            }
+        }
+    }
+    function init()
+    {
+        setUpEventListeners();
+    }
+    init();
+})();
