25bbe07d6f0fd518e23ac9256e7a222a8e606017
gavvahar <nihargavva@gmail.com>
2022-05-09 15:41:07 -0400

    working on the clear button now

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,27 +10,56 @@
         document.querySelector('#tails'),
         document.querySelector('#tails-percent')
     ];
+    scoreBoards(scoreBoard);
+    function handleFlipButton()
+    {
+        const headsOrTails = Math.random() < 0.5;
+        scoreBoard.totalFlips += 1;
+        if(headsOrTails)
+        {
+            scoreBoard.heads += 1;
+            querySelectorArr[2].setAttribute('src', 'assets/images/penny-heads.jpg');
+            querySelectorArr[3].textContent = 'You Flipped Heads';
+        } else
+        {
+            scoreBoard.tails += 1;
+            querySelectorArr[2].setAttribute('src', 'assets/images/penny-tails.jpg');
+            querySelectorArr[3].textContent = 'You Flipped Tails';
+        }
+        scoreBoard.percentageHeads = (scoreBoard.heads / scoreBoard.totalFlips) * 100;
+        scoreBoard.percentageTails = (scoreBoard.tails / scoreBoard.totalFlips) * 100;
+        querySelectorArr[4].textContent = `${scoreBoard.heads}`;
+        querySelectorArr[5].textContent = `${Math.round(scoreBoard.percentageHeads)}%`;
+        querySelectorArr[6].textContent = `${scoreBoard.tails}`;
+        querySelectorArr[7].textContent = `${Math.round(scoreBoard.percentageTails)}%`;
+    }
+    function handleClearButton()
+    {
+        querySelectorArr
+    }
     function setUpEventListeners()
     {
-        for(let i = 0; i < querySelectorArr.length; i++)
-        {
-            switch(i)
-            {
-                case 0:
-                    querySelectorArr[i].addEventListener('click', function(){
-                        console.log('you clicked flip');
-                    });
-                    break;
-                case 1:
-                    querySelectorArr[i].addEventListener('click', function(){
-                        console.log('you clicked clear');
-                    });
-            }
-        }
+        querySelectorArr[0].addEventListener('click', function(){
+            handleFlipButton();
+        });
+        querySelectorArr[1].addEventListener('click', function(){
+            handleClearButton();
+        });
     }
     function init()
     {
         setUpEventListeners();
     }
     init();
+    function scoreBoards(scoreBoard)
+    {
+        let scoreBoard = {
+            heads: 0,
+            percentageHeads: 0,
+            tails: 0,
+            percentageTails: 0,
+            totalFlips: 0,
+        }
+        //return scoreBoard;
+    }
 })();
